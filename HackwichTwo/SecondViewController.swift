36fdd9217a6bdf877fbcadddb7252ef57a9102c4
jf0o <46459398+jf0o@users.noreply.github.com>
2019-01-30 12:26:16 -1000

    Assignment 3: Introduction to Segues, IBOutlets and more on IBAction (not finished)

---
+++ HackwichTwo/SecondViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SecondViewController.swift
+//  HackwichTwo
+//
+//  Created by Jessica Foo  on 1/30/19.
+//  Copyright © 2019 UH West. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var firstLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+     @IBAction func changeColorButtonePressed(_ sender: Any) {
+     
+     //on button press, we want the background color of the view to turn blue
+     self.view.backgroundColor=UIColor.blue
+     
+     //on button press, set firstLabel to the string, "I did it"
+     //hint 1: look up "UI Label"
+     //hint 2: look up attributes of UI Label
+     
+     }
+     }
+    */
+
+}
